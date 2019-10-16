7af0e4adbc25d7e24edf19b8862e3cb74f0b5d01
Guy Langston <guylangston@gmail.com>
2019-10-16 14:35:20 +0100

    Initial version (combined from ConsoleZ and SokoSolve Core)

---
+++ RectInt.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace VectorInt
+{
+
+    public interface IRectInt : IEnumerable<VectorInt2>
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        int W { get; set; }
+        int H { get; set; }
+
+        // Top
+        VectorInt2 TL { get; }
+        VectorInt2 TM { get; }
+        VectorInt2 TR { get; }
+        
+        // Middle
+        VectorInt2 ML { get; }
+        VectorInt2 C { get; }
+        VectorInt2 MR { get; }
+        
+        // Bottom
+        VectorInt2 BL { get; }
+        VectorInt2 BM { get; }
+        VectorInt2 BR { get; }
+    }
+
+    public static class RectExt
+    {
+        public static RectInt Inset(this IRectInt rect, int top, int right, int bottom, int left) 
+            => new RectInt(rect.X + left, rect.Y + top, rect.W - right, rect.H - bottom);
+        
+        public static RectInt Outset(this IRectInt rect, int top, int right, int bottom, int left) 
+            => new RectInt(rect.X - left, rect.Y - top, rect.W + right + left -1, rect.H + top + bottom -1);
+        
+    }
+
+    public class RectInt : IRectInt
+    {
+        public RectInt(int x, int y, int w, int h)
+        {
+            X = x;
+            Y = y;
+            W = w;
+            H = h;
+        }
+
+        public RectInt(VectorInt2 size)
+        {
+            X = 0;
+            Y = 0;
+            W = size.X;
+            H = size.Y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int W { get; set; }
+        public int H { get; set; }
+        
+        public VectorInt2 Size => new VectorInt2(W, H);
+        
+        public VectorInt2 TL => (X, Y);
+        public VectorInt2 TM => (X + W/2, Y);
+        public VectorInt2 TR => (X + W, Y);
+        
+        public VectorInt2 ML => (X, Y + H/2);
+        public VectorInt2 C  => (X + W/2, Y + H/2);
+        public VectorInt2 MR => (X + W, Y + H/2);
+        
+        public VectorInt2 BL => (X, Y + H);
+        public VectorInt2 BM => (X + W/2, Y + H);
+        public VectorInt2 BR => (X + W, Y + H);
+        
+        
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        public IEnumerator<VectorInt2> GetEnumerator()
+        {
+            for (var xx = X; xx < X + W; xx++)
+            for (var yy = Y; yy < Y + H; yy++)
+                yield return new VectorInt2(xx, yy);
+        }
+
+        public static RectInt CenterAt(VectorInt2 at, RectInt rect)
+        {
+            var half = rect.Size / new VectorInt2(2, 2);
+            return new RectInt(at.X - half.X, at.Y - half.Y, rect.W, rect.H);
+        }
+    }
+}
---
+++ VectorInt2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace VectorInt
+{
+    public struct VectorInt2 : IEquatable<VectorInt2>
+    {
+        public VectorInt2(int x, int y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Deconstruct(out int x, out int y)
+        {
+            x = X;
+            y = Y;
+        }
+
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public bool IsUnit => this == Up || this == Down || this == Left || this == Right;
+
+        public static readonly VectorInt2 Zero = new VectorInt2();
+
+        public static readonly VectorInt2 MinValue = new VectorInt2(int.MinValue, int.MinValue);
+        public static readonly VectorInt2 Up = new VectorInt2(0, -1);
+        public static readonly VectorInt2 Down = new VectorInt2(0, 1);
+        public static readonly VectorInt2 Left = new VectorInt2(-1, 0);
+        public static readonly VectorInt2 Right = new VectorInt2(1, 0);
+        public static readonly VectorInt2[] Directions = {Up, Down, Left, Right};
+
+        public static VectorInt2 operator +(VectorInt2 lhs, VectorInt2 rhs) => new VectorInt2(lhs.X + rhs.X, lhs.Y + rhs.Y);
+        public static VectorInt2 operator -(VectorInt2 lhs, VectorInt2 rhs) => new VectorInt2(lhs.X - rhs.X, lhs.Y - rhs.Y);
+        public static VectorInt2 operator *(VectorInt2 lhs, VectorInt2 rhs) => new VectorInt2(lhs.X * rhs.X, lhs.Y * rhs.Y);
+        public static VectorInt2 operator /(VectorInt2 lhs, VectorInt2 rhs) => new VectorInt2(lhs.X / rhs.X, lhs.Y / rhs.Y);
+        public static bool operator ==(VectorInt2 lhs, VectorInt2 rhs) => lhs.Equals(rhs);
+        public static bool operator !=(VectorInt2 lhs, VectorInt2 rhs) => !lhs.Equals(rhs);
+        public static implicit operator  VectorInt2((int x, int y) tuple) => new VectorInt2(tuple.x, tuple.y);
+        
+        public bool Equals(VectorInt2 other) => X == other.X && Y == other.Y;
+
+        public override bool Equals(object obj) => Equals((VectorInt2) obj);
+
+        public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode();
+
+        public override string ToString() => $"({X},{Y})";
+    }
+}
