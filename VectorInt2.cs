31e09f199a6edbc2aee201a7a833aab33e39a3a8
Guy Langston <guylangston@gmail.com>
2019-11-07 16:25:34 +0000

    Vector2 conversion

--- VectorInt2.cs
+++ VectorInt2.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Numerics;
 
 namespace VectorInt
 {
     public struct VectorInt2 : IEquatable<VectorInt2>
     {
+
+        public VectorInt2(Vector2 v) : this()
+        {
+            X = (int)v.X;
+            Y = (int)v.Y;
+        }
+
+
+
+        public VectorInt2(VectorInt2 v) : this()
+        {
+            X = v.X;
+            Y = v.Y;
+        }
+
         public VectorInt2(int x, int y) : this()
         {
             X = x;
@@ -46,7 +62,10 @@ namespace VectorInt
         public static VectorInt2 operator /(VectorInt2 lhs, VectorInt2 rhs) => new VectorInt2(lhs.X / rhs.X, lhs.Y / rhs.Y);
         public static bool operator ==(VectorInt2 lhs, VectorInt2 rhs) => lhs.Equals(rhs);
         public static bool operator !=(VectorInt2 lhs, VectorInt2 rhs) => !lhs.Equals(rhs);
-        public static implicit operator  VectorInt2((int x, int y) tuple) => new VectorInt2(tuple.x, tuple.y);
+        
+        public static implicit operator VectorInt2((int x, int y) tuple) => new VectorInt2(tuple.x, tuple.y);
+        public static implicit operator Vector2(VectorInt2 v) => new Vector2(v.X, v.Y);
+        public static implicit operator VectorInt2(Vector2 v) => new VectorInt2((int)v.X, (int)v.Y);
         
         public bool Equals(VectorInt2 other) => X == other.X && Y == other.Y;
 
